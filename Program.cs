6842c2467ead3aaa318cd51be99c5ccf8fe26626
Christian Tisby <cduane82@hotmail.com>
2019-07-07 20:28:09 -0400

    third commit

--- Program.cs
+++ Program.cs
@@ -10,8 +10,23 @@ namespace BuiltinTypes
             Console.WriteLine($"sbyte: minimum {sbyte.MinValue}, maximum {sbyte.MaxValue}");
             Console.WriteLine($"short: minimum {short.MinValue}, maximum {short.MaxValue}");
 
+            var x = 234;
+            Console.WriteLine(x.GetType());
+
             Console.WriteLine($"int: minimum {int.MinValue}, maximum {int.MaxValue}");
             Console.WriteLine($"long: minimum {long.MinValue}, maximum {long.MaxValue}");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"float: minimum {float.MinValue}, maximum {float.MaxValue}");
+            Console.WriteLine($"double: minimum {double.MinValue}, maximum {double.MaxValue}");
+
+            float f = 123.456789012345678f;
+            double d = 123.456789012345678;
+
+            Console.WriteLine($"f is {f:F99}");
+            Console.WriteLine($"d is {d:F99}");
+
 
 
 
